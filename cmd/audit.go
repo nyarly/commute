db2f53f94bcbed4d78da2a4c0fbd3cb05946d08d
Judson <nyarly@gmail.com>
2019-09-16 08:51:52 -0700

    Tabular output for `audit`

--- cmd/audit.go
+++ cmd/audit.go
@@ -1,6 +1,10 @@
 package cmd
 
 import (
+  "fmt"
+	"os"
+	"text/tabwriter"
+
 	"github.com/spf13/cobra"
 )
 
@@ -11,30 +15,36 @@ var (
 		Short: "review state of the system vs. shared config",
 		Long: longUsage(`Primarily, this prints out directories that seem to refer to a different remote
     than is recorded in the config. Generally, it's best to align remotes.  `),
-		RunE:    auditFn,
+		RunE: auditFn,
 	}
 )
 
 func init() {
+  auditCmd.Flags().Bool("header", true, "print headers for audit table")
 	rootCmd.AddCommand(auditCmd)
 }
 
 func auditFn(cmd *cobra.Command, args []string) error {
-  for _, rem := range cfg.Remotes {
-    path, err := rem.localPath()
-    if err != nil {
-      continue
-    }
-
-    remotes, err := getRemotes(path)
-    if err != nil {
-      return err
-    }
-
-    picked, same := chooseRemote(cfg, remotes)
-    if !same {
-      normal("%q: commute has %q but git remote is %q", path, rem, picked)
-    }
+	tabs := tabwriter.NewWriter(os.Stdout, 0, 4, 2, ' ', 0)
+  if header, err := cmd.Flags().GetBool("header"); err == nil && header {
+    fmt.Fprintln(tabs, "PATH\tCONFIGURED\tGIT REMOTE")
   }
-  return nil
+	for _, rem := range cfg.Remotes {
+		path, err := rem.localPath()
+		if err != nil {
+			continue
+		}
+
+		remotes, err := getRemotes(path)
+		if err != nil {
+			return err
+		}
+
+		picked, same := chooseRemote(cfg, remotes)
+		if !same {
+			fmt.Fprintf(tabs, "%s\t%s\t%s\n", path, rem, picked)
+		}
+	}
+  tabs.Flush()
+	return nil
 }
