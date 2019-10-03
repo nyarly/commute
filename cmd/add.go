313f03c61c9508ab45abdfaf5458d07be0f18409
Judson <nyarly@gmail.com>
2019-10-03 16:16:12 -0700

    Add chooses named remote

--- cmd/add.go
+++ cmd/add.go
@@ -4,13 +4,14 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
 
 func init() {
 	rootCmd.AddCommand(addCmd)
-  addCmd.Flags().StringP("remote", "r", "", "specify a remote to add, rather than guess")
+	addCmd.Flags().StringP("remote", "r", "", "specify a remote to add, rather than guess")
 }
 
 var addCmd = &cobra.Command{
@@ -28,25 +29,31 @@ func addFn(cmd *cobra.Command, args []string) error {
 		return err
 	}
 
-  remotes, err := getRemotes(root)
+	remotes, err := getRemotes(root)
 	if err != nil {
 		return err
 	}
 
-  rem, found := chooseRemote(cfg, remotes)
+	var rem remote
+	var found bool
 
-  chosenRemote, err :=  cmd.Flags().GetString("remote")
-  if err != nil {
-    return err
-  }
-
-  for _, r := range remotes {
-    if chosenRemote == r.name {
-      rem = remote(r.url)
+	if cmd.Flags().Changed("remote") {
+		chosenRemote, err := cmd.Flags().GetString("remote")
+		if err != nil {
+			return err
+		}
+    gr, err := pickNamedRemote(chosenRemote, remotes)
+    if err != nil {
+      return err
     }
-  }
+    rem, found = chooseRemote(cfg, []gitRemote{gr})
+
+	} else {
+		rem, found = chooseRemote(cfg, remotes)
+	}
 
 	if !found {
+		verbose("New remote %q added to configuration\n", rem)
 		cfg.Remotes = append(cfg.Remotes, rem)
 	}
 
@@ -62,4 +69,19 @@ func addFn(cmd *cobra.Command, args []string) error {
 	}
 	os.Mkdir(filepath.Dir(lp), os.ModeDir|os.ModePerm)
 	return os.Symlink(root, lp)
+}
+
+func pickNamedRemote(chosenRemote string, remotes []gitRemote) (gitRemote, error) {
+  for _, r := range remotes {
+    if chosenRemote == r.name {
+      return r, nil
+    }
+  }
+
+  remnames := []string{}
+  for _, r := range remotes {
+    remnames = append(remnames, r.name)
+  }
+
+  return gitRemote{}, fmt.Errorf("no remote named %q in local repo - found: %s", chosenRemote, strings.Join(remnames, ", "))
 }
